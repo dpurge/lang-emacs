f4d71cd5318b14087762ecff7e27d19b1c4fb895
D. Purge <david.purge@gmail.com>
2024-01-03 21:57:35 +0100

    Save temp work

--- jdp-custom.el
+++ jdp-custom.el
@@ -25,6 +25,34 @@
 
 (require 'jdp-lang)
 
+;; dictionary tests
+;; https://github.com/myrkr/dictionary-el
+
+
+
+(setopt
+  dictionary-search-interface 'help
+  dictionary-default-strategy "prefix"
+  dictionary-default-dictionary "fd-eng-hin"
+  dictionary-server "dict.org")
+
+(keymap-global-set "M-\\" #'dictionary-search)
+;(global-set-key (kbd "C-c l") #'dictionary-lookup-definition)
+
+;(setq dictionary-server "dict.org")
+;(setq dictionary-use-single-buffer t)
+;(setq dictionary-default-dictionary "fd-eng-ara")
+
+;(add-hook 'text-mode-hook 'dictionary-tooltip-mode)
+;(setq switch-to-buffer-obey-display-actions t)
+;(add-to-list 'display-buffer-alist
+;   '("^\\*Dictionary\\*" display-buffer-in-side-window
+;     (side . right)
+;     (window-width . 40)))
+
+;(setq dictionary-tooltip-dictionary "fd-eng-ara")
+;(global-dictionary-tooltip-mode 1)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
